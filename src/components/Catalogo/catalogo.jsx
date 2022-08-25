fea3067ff38ca59514a28931af0839c4e5fe7c52
Daniel Marinho <70353676+D4NL18@users.noreply.github.com>
2022-08-25 19:00:38 -0300

    search

--- src/components/Catalogo/catalogo.jsx
+++ src/components/Catalogo/catalogo.jsx
@@ -11,13 +11,14 @@ export default function Catalogo() {
     const [posts, setPosts] = useState([]);
     const [page, setPage] = useState(1)
     const [totalPages, setTotalPages] = useState(0)
-    const PATH = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=0c70d0600937fa6b2433c507fd200985'
+    const PATH = `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=0c70d0600937fa6b2433c507fd200985&page=${page}`
+    const SEARCH = `https://api.themoviedb.org/3/search/movie?api_key=0c70d0600937fa6b2433c507fd200985&query=${searchTerm}&page=${page}`
 
 
     useEffect(() => {
 
         const fetchPosts = async () => {
-            const res = await axios.get(PATH)
+            const res = await axios.get((searchTerm) ? SEARCH : PATH)
             setPosts(res.data.results)
             setTotalPages(res.data.total_pages)
         }
--- src/components/Posts/posts.jsx
+++ src/components/Posts/posts.jsx
@@ -4,16 +4,7 @@ import Styles from './posts.module.css'
 export default function Posts({ posts, searchTerm }) {
     return (
         <div className={Styles.posts}>
-            {posts.filter((val) => {
-                if (searchTerm === "") {
-                    return val
-                }
-                else if (val.title.toLowerCase().includes(searchTerm.toLowerCase())) {
-                    return val
-                } else {
-                    return null
-                }
-            }).map((val, key) => {
+            {posts.map((val, key) => {
                 return (
                     <Item
                         text={val.title}
